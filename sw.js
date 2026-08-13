c44d1718d6b72a575a29b3d204b9a6281f132163
joeoreilly79 <joe@oraa.ie>
2026-08-13 07:49:07 +0100

    Bump cache version to v2 to force refresh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "oraa-todo-v1";
+const CACHE = "oraa-todo-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
